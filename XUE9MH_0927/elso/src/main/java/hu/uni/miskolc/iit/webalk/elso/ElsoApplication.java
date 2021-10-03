225480e44b49697a1fc0be48812cd159cd925c4d
SDaniel626 <simon.daniel.xue9mh@gmail.com>
2021-10-03 20:54:07 +0200

    WIP

---
+++ XUE9MH_0927/elso/src/main/java/hu/uni/miskolc/iit/webalk/elso/ElsoApplication.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit.webalk.elso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsoApplication.class, args);
+	}
+
+}
